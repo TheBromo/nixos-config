06c786b4d51ff31cbaba79a990e40ba70c9debc3
thebromo <manuel@strenge.ch>
2024-03-03 12:37:12 +0100

    added emojis

--- hosts/zephyrus/default.nix
+++ hosts/zephyrus/default.nix
@@ -115,12 +115,29 @@
   # documentation for this option (e.g. man configuration.nix or on 
   # https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
-
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
   };
   hardware.opengl.enable = true;
 
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      font-awesome
+      source-han-sans
+      open-sans
+      source-han-sans-japanese
+      source-han-serif-japanese
+    ];
+    fontconfig.defaultFonts = {
+      serif = [ "Noto Serif" "Source Han Serif" ];
+      sansSerif = [ "Open Sans" "Source Han Sans" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
   #environment = {
   #  shells = [ pkgs.zsh ];
   #};
