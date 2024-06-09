755bd6e44d03674bf9a07cc481cf1fcfc6275f52
thebromo <manuel@strenge.ch>
2024-06-09 13:16:10 +0200

    added gh

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -1,10 +1,29 @@
-{ pkgs, ... }: {
-
-  programs.git-credential-oauth.enable = true;
+{ pkgs, lib, ... }: {
+  programs.gh.enable = true;
 
   programs.git = {
     enable = true;
     userName = "thebromo";
     userEmail = "manuel@strenge.ch";
+    lfs.enable = true;
+    extraConfig = {
+      init.defaultBranch = "main";
+      pull.rebase = "true";
+      delta.enable = true;
+
+      credential."https://github.com".helper = "${lib.getExe pkgs.gh} auth git-credential";
+      credential."https://gist.github.com".helper = "${lib.getExe pkgs.gh} auth git-credential";
+      credential."https://github.zhaw.ch".helper = "${lib.getExe pkgs.gh} auth git-credential";
+    };
+  };
+
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      git.paging = {
+        colorArg = "always";
+        pager = "${lib.getExe pkgs.delta} --dark --paging=never";
+      };
+    };
   };
 }
