ce1e07f8da6ea256a7a738a9c8f3be449004278c
thebromo <manuel@strenge.ch>
2024-06-17 18:35:48 +0200

    fixed boot menu

--- hosts/casa/default.nix
+++ hosts/casa/default.nix
@@ -22,7 +22,11 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
