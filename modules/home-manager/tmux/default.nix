2b74f488d8809945630463d1f1d43db462e1390c
thebromo <manuel@strenge.ch>
2024-06-26 11:22:15 +0000

    adjusted tmux

--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -8,11 +8,11 @@ in
     baseIndex = 1;
     secureSocket = true;
     mouse = true;
+    keyMode = "vi";
     clock24 = true;
     plugins = with pkgs.tmuxPlugins;[
       pain-control
       vim-tmux-navigator
-      sensible
       yank
     ];
     extraConfig = ''
@@ -21,10 +21,29 @@ in
       set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
       set-environment -g COLORTERM "truecolor"
 
-      set -g status-style 'bg=#181616 fg=#c5c9c5'
-      set -g mode-keys vi
+      bind C-s set-option -g status
 
       bind-key -r f run-shell "tmux neww tmux-sessionizer"
+
+      # Set the left side of the status bar
+      set -g status-left-length 20
+      set -g status-left " ▲ #S | "
+      set -g status-left-style fg=black,bg=green
+
+      # Set the right side of the status bar
+      set -g status-right " %H:%M %d-%b-%y "
+      set -g status-right-style fg=green,bg=black
+
+      set -g status-bg black
+      set -g status-fg green
+
+      # Set the window status format
+      set -g window-status-format " #I: #W "
+
+      # Set the current window status format
+      set -g window-status-current-format " #I: #W "
+      set -g window-status-current-style fg=black,bg=green
+
     '';
 
   };
