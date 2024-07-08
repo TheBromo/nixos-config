b18729a327c635760b488f6cf8a8911bdca98390
thebromo <manuel@strenge.ch>
2024-07-08 21:48:09 +0200

    format

--- pkgs/dvt/default.nix
+++ pkgs/dvt/default.nix
@@ -1,18 +1,18 @@
 { pkgs, ... }:
 pkgs.writeScriptBin "dvt" ''
   #!${pkgs.bash}/bin/bash
-if [ -z $1 ]; then
-  echo "no template specified"
-  exit 1
-fi
+  if [ -z $1 ]; then
+    echo "no template specified"
+    exit 1
+  fi
 
-TEMPLATE=$1
+  TEMPLATE=$1
 
-nix \
-  --experimental-features 'nix-command flakes' \
-  flake init \
-  --template \
-  "github:the-nix-way/dev-templates#''${TEMPLATE}"
+  nix \
+    --experimental-features 'nix-command flakes' \
+    flake init \
+    --template \
+    "github:the-nix-way/dev-templates#''${TEMPLATE}"
 
   direnv allow
 ''
