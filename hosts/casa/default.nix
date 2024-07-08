639f8bc8fc7dbd0ce7546ff3296ebfe37264b63f
thebromo <manuel@strenge.ch>
2024-07-08 21:20:41 +0200

    limited configs

--- hosts/casa/default.nix
+++ hosts/casa/default.nix
@@ -28,6 +28,7 @@
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.configurationLimit = 5;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
