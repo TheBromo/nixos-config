9d477bb9b27f5118c5179e60454e4535d7ae2860
thebromo <manuel@strenge.ch>
2024-07-09 09:19:56 +0000

    fixed version dvt

--- pkgs/dvt/default.nix
+++ pkgs/dvt/default.nix
@@ -1,5 +1,6 @@
-{ pkgs, ... }:
-pkgs.writeScriptBin "dvt" ''
+{ pkgs, ... }:let 
+    githash = "d5ad3c909c004f202019f662bf7304ee59f6b5e4";
+in pkgs.writeScriptBin "dvt" ''
   #!${pkgs.bash}/bin/bash
   if [ -z $1 ]; then
     echo "no template specified"
@@ -12,7 +13,7 @@ pkgs.writeScriptBin "dvt" ''
     --experimental-features 'nix-command flakes' \
     flake init \
     --template \
-    "github:the-nix-way/dev-templates#''${TEMPLATE}"
+    "github:the-nix-way/dev-templates/${githash}#''${TEMPLATE}"
 
   direnv allow
 ''
