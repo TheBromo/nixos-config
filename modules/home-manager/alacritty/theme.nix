84ccee09db7c69ec7d06b098ab67febd9e64f5a3
thebromo <manuel@strenge.ch>
2024-08-04 21:58:46 +0200

    updated

--- modules/home-manager/alacritty/theme.nix
+++ modules/home-manager/alacritty/theme.nix
@@ -1,42 +1,29 @@
 {
   draw_bold_text_with_bright_colors = true;
-
-  bright = {
-    black = "#949494";
-    blue = "#74b2ff";
-    cyan = "#85dc85";
-    green = "#36c692";
-    magenta = "#ae81ff";
-    red = "#ff5189";
-    white = "#e4e4e4";
-    yellow = "#c6c684";
-  };
-
-  cursor = {
-    cursor = "#8e8e8e";
-    text = "#080808";
+  primary = {
+    background = "#080808";
+    foreground = "#fff1f3";
   };
 
   normal = {
-    black = "#323437";
-    blue = "#80a0ff";
-    cyan = "#79dac8";
-    green = "#8cc85f";
-    magenta = "#cf87e8";
-    red = "#ff5454";
-    white = "#c6c6c6";
-    yellow = "#e3c78a";
+    black = "#121212";
+    red = "#fd6883";
+    green = "#adda78";
+    yellow = "#f9cc6c";
+    blue = "#f38d70";
+    magenta = "#a8a9eb";
+    cyan = "#85dacc";
+    white = "#fff1f3";
   };
 
-  primary = {
-    background = "#080808";
-    bright_foreground = "#eeeeee";
-    foreground = "#bdbdbd";
+  bright = {
+    black = "#72696a";
+    red = "#fd6883";
+    green = "#adda78";
+    yellow = "#f9cc6c";
+    blue = "#f38d70";
+    magenta = "#a8a9eb";
+    cyan = "#85dacc";
+    white = "#fff1f3";
   };
-
-  selection = {
-    background = "#b2ceee";
-    text = "#080808";
-  };
-
 }
--- modules/home-manager/tmux/default.nix
+++ modules/home-manager/tmux/default.nix
@@ -16,11 +16,6 @@ in
       vim-tmux-navigator
     ];
     extraConfig = ''
-      set -g default-terminal "xterm-256color"
-      set -ga terminal-overrides ",*256col*:Tc"
-      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
-      set-option -sa terminal-overrides ",xterm*:Tc"
-
       set -g default-terminal "alacritty" 
       set-option -sa terminal-overrides ",alacritty*:Tc"
 
@@ -36,22 +31,30 @@ in
       # Set the left side of the status bar
       set -g status-left-length 30
       set -g status-left " ▲ #S | "
-      set -g status-left-style fg="#303030",bg="#36c692"
+      set -g status-left-style fg="black",bg="green"
 
       # Set the right side of the status bar
       set -g status-right " %H:%M %d-%b-%y "
-      set -g status-right-style fg="#36c692",bg="#303030"
+      set -g status-right-style fg="green",bg="black"
 
-      set -g status-bg "#303030"
-      set -g status-fg "#36c692"
+      set -g status-bg "black"
+      set -g status-fg "green"
 
 
       # Set the window status format
-      set -g window-status-format " #I:[#W]#(basename #{pane_current_path}| cut -c1-5) "
+      # Set a custom window status format based on the current pane's program
+      set -g window-status-format " #I:#( \
+        if [ \"#{pane_current_command}\" = \"bash\" ]; then \
+          basename \"#{pane_current_path}\" | cut -c1-10; \
+        else \
+          echo \"#{pane_current_command}\"; \
+        fi \
+      ) "
+
 
       # Set the current window status format
-      set -g window-status-current-format " #I:[#W]#(basename #{pane_current_path}| cut -c1-10) "
-      set -g window-status-current-style fg="#303030",bg="#36c692"
+      set -g window-status-current-format " #I:#(basename #{pane_current_path}| cut -c1-10) "
+      set -g window-status-current-style fg="black",bg="green"
 
     '';
   };
