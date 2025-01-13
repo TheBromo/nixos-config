d77158c0806f44c4ff6401f490c84074fb67909b
thebromo <manuel@strenge.ch>
2025-01-13 20:40:35 +0100

    changed config to be mutable

--- modules/home-manager/nvim-config/default.nix
+++ modules/home-manager/nvim-config/default.nix
@@ -1,30 +1,17 @@
-{pkgs,lib,...}:
-let
-  neovimConfigRepo = pkgs.fetchgit {
-      url = "https://github.com/TheBromo/neovim-config.git";
-    #rev = "310609e"; # Adjust to the correct branch or tag
-      sha256 = "sha256-lUYYEhzOgViBw3dDrGytK1p53GEHTpvULYmsa6K3itk=";
-  };
-
-in
-{
+{pkgs,lib,...}:{
 	home.packages =[
 		pkgs.neovim
 		pkgs.git
-		pkgs.rsync
 	];
 
 	  home.activation = {
     configureNvim = lib.mkAfter ''
-      # Ensure ~/.config/nvim exists
       mkdir -p ~/.config/nvim
-
-      # Sync fetched repository to the directory if empty
-      if [ ! -d ~/.config/nvim/.git ]; then
-        ${pkgs.rsync}/bin/rsync -a ${neovimConfigRepo}/ ~/.config/nvim/
+      if [ -z "$(ls -A ~/.config/nvim)" ] || [ ! -d ~/.config/nvim/.git ]; then
+	${pkgs.git}/bin/git clone https://github.com/TheBromo/neovim-config.git ~/.config/nvim
+      else
+        echo "Neovim configuration already exists. Skipping clone."
       fi
-
-      # Set ownership and permissions for the 'manuel' user
       chown -R manuel:manuel ~/.config/nvim
       chmod -R u+w ~/.config/nvim
     '';
