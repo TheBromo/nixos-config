9629e88bf20145a970220e24f24420deeb5b7552
manuel <manuel@strenge.ch>
2025-03-20 10:09:59 +0100

    added sdkman

--- modules/home-manager/console/default.nix
+++ modules/home-manager/console/default.nix
@@ -63,6 +63,7 @@ in {
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
       [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+      source "$HOME/.sdkman/bin/sdkman-init.sh"
       source <(kubectl completion zsh)
       export GOPATH=$HOME/go
       screenfetch
