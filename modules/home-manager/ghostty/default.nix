99b89a9c139b4dff37f2260dfd237096eb04ef98
--list <manuel.strenge-ext@hexagon.com>
2026-03-26 14:01:03 +0100

    fix icon

--- modules/home-manager/ghostty/default.nix
+++ modules/home-manager/ghostty/default.nix
@@ -11,18 +11,25 @@
       xdg.desktopEntries.ghostty = {
         name = "Ghostty";
         genericName = "Terminal Emulator";
-        exec = "${config.lib.nixGL.wrap pkgs.ghostty}/bin/ghostty";
-        icon = "com.mitchellh.ghostty";
+        exec = "${
+          config.lib.nixGL.wrap inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default
+        }/bin/ghostty";
+        icon = "${
+          inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default
+        }/share/icons/hicolor/256x256/apps/com.mitchellh.ghostty.png";
         terminal = false;
         type = "Application";
         categories = [
           "System"
           "TerminalEmulator"
         ];
+        settings.StartupWMClass = "com.mitchellh.ghostty";
       };
 
       programs.ghostty = {
-        package = (config.lib.nixGL.wrap inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default);
+        package = (
+          config.lib.nixGL.wrap inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default
+        );
         enable = true;
         enableZshIntegration = true;
         installVimSyntax = true;
@@ -33,7 +40,7 @@
             "TX-02"
             "Apple Color Emoji"
           ];
-          font-size = 12.5;
+          font-size = 13;
           font-feature = "-calt, -liga, -dlig";
 
           shell-integration = "zsh";
